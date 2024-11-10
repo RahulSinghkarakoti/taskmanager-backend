2bd4f915f4284679b7c25599d81ee37c9c8a613e
John Doe <john.doe@contoso.com>
2024-11-10 13:06:20 +0530

    add expired task logic

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express, { json } from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
+import cron from "node-cron"
+import { Task } from "./models/task.model.js"
 const app=express()
 
 //use method is used to implement midelwears (cors,cookieparse)
@@ -20,6 +22,25 @@ app.use(express.static('public'))
 app.use(bodyParser.json());
 
 app.use(cookieParser());//use to perform CRUD operation on cookies_at user device
+
+// Schedule the cron job (e.g., every hour)
+cron.schedule('0 * * * *', async () => {
+  try {
+    console.log("in expire module")
+    const now = new Date();
+
+    // Update tasks where the deadline has passed
+    const result = await Task.updateMany(
+      { deadline: { $lt: now }, status: { $ne: 'expired' } },
+      { $set: { status: 'expired' } }
+    );
+
+    console.log(`Expired tasks updated: ${result.modifiedCount}`);
+  } catch (error) {
+    console.error('Error marking tasks as expired:', error);
+  }
+});
+
 
 // //import routes
 import TaskRouter from "./routes/task.route.js" 
