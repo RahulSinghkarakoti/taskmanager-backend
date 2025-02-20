c8fe0945187fcb17ea7aa7ee520a4b34071dc306
rahul karakoti <rahulsinghkarakoti28@gmail.com>
2025-02-20 16:16:26 +0530

    remove nodemodule

--- src/db/index.js
+++ src/db/index.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
 
 const connectDB=async ()=>{
+    const URL=process.env.MONOGO_URI;
+    // console.log(URL)
     try {
-       const connectionInstance = await mongoose.connect(`${process.env.MONOGO_URI}/${DB_NAME}`)
+       const connectionInstance = await mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
        console.log(`\n MongoDB connected !! DB HOST :${connectionInstance.connection.host}`)
 
     } catch (error) {
